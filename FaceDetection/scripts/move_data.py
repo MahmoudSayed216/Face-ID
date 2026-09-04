23d97d104237351f58fcc511641a0c3ed85e7a3d
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-09-04 15:12:14 +0300

    modified .gitkeep

---
+++ FaceDetection/scripts/move_data.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+
+
+
+
+def main():
+
+    SRC_DIR = "/kaggle/input/datasets/iamprateek/wider-face-a-face-detection-dataset/WIDER_train/WIDER_train/images"
+    KEYS_DIR = "/kaggle/working/Face-ID/FaceDetection/data"
+    DST_DIR = ""
+
+if __name__ == "__main__":
+    main()
