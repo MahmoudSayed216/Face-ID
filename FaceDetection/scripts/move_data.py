862be84ee334e6eba9899003160fafabe7c07ecc
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-09-04 16:42:34 +0300

    created yaml file for data

--- FaceDetection/scripts/move_data.py
+++ FaceDetection/scripts/move_data.py
@@ -26,7 +26,7 @@ def move_data(src_dir: str, dst_dir: str, keys: list, split: str):
     try:
         dst_dir = os.path.join(dst_dir, split)
         print(f"moving {split} to {dst_dir}")
-        print(dst_dir)
+        # print(dst_dir)
         for key in keys:
             img_path = os.path.join(src_dir, key)
             shutil.copy(img_path, dst_dir)
